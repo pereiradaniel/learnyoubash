ae17b2f1ac582b92dbf7ccdea4961af16966c9c0
Daniel Pereira <pereiradpg@yahoo.ca>
2017-09-28 12:18:57 -0400

    case conditional statement passed

---
+++ case_conditional_statement/case.bash
@@ -0,0 +1,13 @@
+case "$1" in
+  ("jpg"|"jpeg")
+    echo 'It is jpeg.'
+  ;;
+  "png")
+    echo 'It is png.'
+  ;;
+  "gif")
+    echo 'It is gif.'
+  ;;
+  *)
+    echo "$1 is not an image!"
+esac
